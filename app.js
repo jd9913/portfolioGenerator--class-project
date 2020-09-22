5aeff27a01150ce8f09dbf30c9318c959fe610e2
Jennifer Schlierman <jd9913@gmail.com>
2020-09-21 20:23:23 -0700

    recreating the original code

--- app.js
+++ app.js
@@ -13,5 +13,5 @@ const [name, github] = profileDataArgs;
 fs.writeFile('./index.html', generatePage(name, github), err => {
     if (err) throw new Error (err);
 
-    console.log('Portfolio complete! Check out index.html to see the output!')
+    console.log('Portfolio complete! Check out index.html to see the output!');
 });
